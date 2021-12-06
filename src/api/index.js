438415627b83022ca41d4cb4e3b4436c43b56743
Ramon Audain <raudain123@gmail.com>
2021-12-06 13:13:24 -0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -9,25 +9,7 @@ export const todaysPic = async () => {
   
     try {
       const res = await axios.get(`${url}`);
-    //   console.log(res.data)
-    //   res.data.forEach((e) => {
-    //     if (e.country !== "US") {
-    //       const modified = {
-    //         province: e.province,
-    //         county: e.county,
-    //         country: e.country,
-    //         confirmed: e.stats.confirmed,
-    //         deaths: e.stats.deaths,
-    //         recovered: e.stats.recovered,
-    //         latitude: e.coordinates.latitude,
-    //         longitude: e.coordinates.longitude,
-    //       };
-    
-    //       data.push(modified);
-    //     }
-    
-  
-        return res.data;
+      return res.data;
     } catch (err) {
       console.error(err);
     }
